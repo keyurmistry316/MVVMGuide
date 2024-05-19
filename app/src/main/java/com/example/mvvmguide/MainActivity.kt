8b061aa145234bb82a6c535addc09e9b4cac1331
keyurmistry316 <99206977+keyurmistry316@users.noreply.github.com>
2024-05-19 14:07:02 -0700

    remove edge to edge

--- app/src/main/java/com/example/mvvmguide/MainActivity.kt
+++ app/src/main/java/com/example/mvvmguide/MainActivity.kt
@@ -7,6 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
     }
 }
