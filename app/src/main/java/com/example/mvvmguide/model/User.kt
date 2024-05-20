fb77b30865655d44e2a195e3b906b7165721a15d
keyurmistry316 <99206977+keyurmistry316@users.noreply.github.com>
2024-05-20 13:42:41 -0700

    -setup room database

--- app/src/main/java/com/example/mvvmguide/model/User.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mvvmguide.model
-
-data class User(
-    val id:Long,
-    val name:String
-)
---
+++ app/src/main/java/com/example/mvvmguide/repo/dao/ShoppingDao.kt
@@ -0,0 +1,23 @@
+package com.example.mvvmguide.repo.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.mvvmguide.model.ShoppingItem
+
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(item: ShoppingItem)
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shoppingitem")
+    suspend fun getItems():LiveData<List<ShoppingItem>>
+}
